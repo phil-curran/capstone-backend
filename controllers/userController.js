aadc5658f0ef024bf59cd43e9c2366c2a286b4b2
Phil Curran <pecurran@outlook.com>
2024-03-25 14:24:13 -0700

    implemented jsonwebtoken.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 import UserModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -9,11 +14,12 @@ const loginUser = async (req, res) => {
 // singup user
 // sign up user
 const registerUser = async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
   try {
     const user = await UserModel.register(email, password);
-    res.status(200).json({ email, user });
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
